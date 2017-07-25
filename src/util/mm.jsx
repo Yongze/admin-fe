6346d2193e1f2284a98317e0946296e32b84e8c7
Yongze <yw850@outlook.com>
2017-07-26 03:30:50 +1000

    fix hard code host name to yw850

--- src/util/mm.jsx
+++ src/util/mm.jsx
@@ -9,10 +9,10 @@
 
 const conf = {
     // online
-    // serverHost: 'http://admin.happymmall.com'
+    // serverHost: 'http://admin.yw850.com'
     // dev
     serverHost: '',
-    imageHost: 'http://img.happymmall.com/',
+    imageHost: 'http://img.yw850.com/',
 }
 
 class MMUtil{
